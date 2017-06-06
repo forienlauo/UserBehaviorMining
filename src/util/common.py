9a6051074b7e6676926467232c1c2398167b154f
forienlauo <244382021@qq.com>
2017-06-06 15:09:29 +0800

    Support setting precision of new_proportion

--- src/util/common.py
+++ src/util/common.py
@@ -1,5 +1,12 @@
+# coding=utf-8
 import random
 
 
-def new_proportion():
-    return 1.0 * random.randint(0, 99) / 100
+def new_proportion(precision=100):
+    """生成一个概率 p, 精度为 1/precision
+    :param precision: int, 大于等于1
+    :return:
+    """
+    precision = int(precision)
+    assert precision >= 1
+    return 1.0 * random.randint(0, precision - 1) / precision
