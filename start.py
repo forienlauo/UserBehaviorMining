7374511fe2a678b7f833c4e55e03db2f4d36ee0f
forienlauo <244382021@qq.com>
2017-06-07 15:07:59 +0800

    Support model persistence

--- start.py
+++ start.py
@@ -2,8 +2,8 @@
 import sys
 
 import conf
-import train
-import util.example_utils as example
+from train import CNNTrainer
+from util.example_utils import ExampleAllocator
 
 if __name__ == '__main__':
     generatedDataFilePaths = ('resource/little_data/data_x.txt', 'resource/little_data/data_y.txt',)
@@ -17,19 +17,21 @@ if __name__ == '__main__':
         # 'tmp/cnn_test/test_data_x.in', 'tmp/cnn_test/test_data_y.in',
         # 5 * 60, 20, 1, 2,
         # 200, 10,
+        # 'tmp/model_persistence/cnn_test.model',
         # little data
         ',',
         trainDataFilePaths[0], trainDataFilePaths[1],
         testDataFilePaths[0], testDataFilePaths[1],
         60, 36, 1, 2,
         1000, 20,
+        'resource/little_data/model/cnn.model',
     ])
     argv = map(lambda x: str(x), sys.argv)
 
     conf.init()
 
-    example.ExampleAllocator(
-        generatedDataFilePaths,
-        trainDataFilePaths=trainDataFilePaths, testDataFilePaths=testDataFilePaths,
-    ).allocate()
-    train.cnn(argv)
+    # ExampleAllocator(
+    #     generatedDataFilePaths,
+    #     trainDataFilePaths=trainDataFilePaths, testDataFilePaths=testDataFilePaths,
+    # ).allocate()
+    CNNTrainer.fit(argv)
