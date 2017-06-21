67184ceaece00dbd4d9fce05faf01f0af7607b34
forienlauo <244382021@qq.com>
2017-06-21 11:35:43 +0800

    Support model visualization

--- src/train.py
+++ src/train.py
@@ -58,9 +58,10 @@ class CNNTrainer(object):
 
         # Construct
         # input and labels
-        x = tf.placeholder(tf.float32, shape=[None, initial_height * initial_width], name='x', )
-        y_ = tf.placeholder(tf.float32, shape=[None, target_class_cnt], name="y_", )
-        keep_prob = tf.placeholder(tf.float32, name='keep_prob', )
+        with tf.name_scope('input') as _:
+            x = tf.placeholder(tf.float32, shape=[None, initial_height * initial_width], name='x', )
+            y_ = tf.placeholder(tf.float32, shape=[None, target_class_cnt], name="y_", )
+            keep_prob = tf.placeholder(tf.float32, name='keep_prob', )
         # trainer and evaluator
         train_per_step, accuracy = CNNTrainer.construct(
             initial_height, initial_width, initial_channels, target_class_cnt,
@@ -130,6 +131,8 @@ class CNNTrainer(object):
             logging.info('total data: %d' % (test_data_len,))
             logging.info("final test accuracy %g" % (final_accuracy,))
 
+        return 0
+
     @staticmethod
     def construct(
             initial_height, initial_width, initial_channels, target_class_cnt,
@@ -155,77 +158,102 @@ class CNNTrainer(object):
             # 如果使用默认步长strides[height, width]=[2,2], 则卷积后的图片大小变为原来的一半
             return tf.nn.max_pool(x, ksize, strides, padding, name=name, )
 
-        # First Convolutional Layer
-        _height1, _width1 = initial_height, initial_width
-        _in_channels1 = initial_channels
-        x_image = tf.reshape(x, [-1, _height1, _width1, _in_channels1], name='x_image', )
-        in1 = x_image  # shape[_example_cnt, _height1, _width1, _in_channels1]
-        _out_channels1 = 32
+        with tf.name_scope('model') as _:
+            # Input Layer
+            with tf.name_scope('InputLayer') as _:
+                in0 = x
+                x_image = tf.reshape(in0, [-1, initial_height, initial_width, initial_channels], name='x_image', )
+                out0 = x_image
 
-        W_conv1 = weight_variable(
-            [CNNTrainer.CONV_HEIGHT, CNNTrainer.CONV_WIDTH, _in_channels1, _out_channels1], name='W_conv1', )
-        b_conv1 = bias_variable([_out_channels1], name='b_conv1', )
+            # C1
+            with tf.name_scope('C1') as _:
+                in1 = out0  # shape[_example_cnt, _height1, _width1, _in_channels1]
+                _height1, _width1 = in1.get_shape()[1].value, in1.get_shape()[2].value
+                _in_channels1 = in1.get_shape()[3].value
+                _out_channels1 = 32
 
-        h_conv1 = tf.nn.relu(conv2d(in1, W_conv1) + b_conv1, name='h_conv1', )
-        h_pool1 = max_pool(h_conv1, CNNTrainer.POOL_SHAPE, name='h_pool1', )
+                W_conv1 = weight_variable(
+                    [CNNTrainer.CONV_HEIGHT, CNNTrainer.CONV_WIDTH, _in_channels1, _out_channels1], name='W_conv1', )
+                b_conv1 = bias_variable([_out_channels1], name='b_conv1', )
+                h_conv1 = tf.nn.relu(conv2d(in1, W_conv1) + b_conv1, name='h_conv1', )
 
-        out1 = h_pool1
+                out1 = h_conv1
 
-        # Second Convolutional Layer
-        in2 = out1  # shape[_example_cnt, _height2, _width2, _in_channels2]
-        _height2, _width2 = out1.get_shape()[1].value, out1.get_shape()[2].value
-        _in_channels2 = out1.get_shape()[3].value
-        _out_channels2 = 64
+            # S2
+            with tf.name_scope('S2') as _:
+                in2 = out1
 
-        W_conv2 = weight_variable(
-            [CNNTrainer.CONV_HEIGHT, CNNTrainer.CONV_WIDTH, _in_channels2, _out_channels2], name='W_conv2', )
-        b_conv2 = bias_variable([_out_channels2], name='b_conv2', )
+                h_pool2 = max_pool(in2, CNNTrainer.POOL_SHAPE, name='h_pool2', )
 
-        h_conv2 = tf.nn.relu(conv2d(in2, W_conv2) + b_conv2, name='h_conv2', )
-        h_pool2 = max_pool(h_conv2, CNNTrainer.POOL_SHAPE, name='h_pool2', )
+                out2 = h_pool2
 
-        out2 = h_pool2
+            # C3
+            with tf.name_scope('C3') as _:
+                in3 = out2  # shape[_example_cnt, _height2, _width2, _in_channels2]
+                _height2, _width2 = in3.get_shape()[1].value, in3.get_shape()[2].value
+                _in_channels2 = in3.get_shape()[3].value
+                _out_channels2 = 64
 
-        # Densely Connected Layer(Full Connected Layer)
-        in3 = out2  # shape[_example_cnt, _height3, _width3, _in_channels3]
-        _height3, _width3 = in3.get_shape()[1].value, in3.get_shape()[2].value
-        _in_channels3 = in3.get_shape()[3].value
-        _out_width3 = 1024
+                W_conv2 = weight_variable(
+                    [CNNTrainer.CONV_HEIGHT, CNNTrainer.CONV_WIDTH, _in_channels2, _out_channels2], name='W_conv2', )
+                b_conv2 = bias_variable([_out_channels2], name='b_conv2', )
+                h_conv2 = tf.nn.relu(conv2d(in3, W_conv2) + b_conv2, name='h_conv2', )
 
-        W_fc1 = weight_variable([_height3 * _width3 * _in_channels3, _out_width3], name='W_fc1', )
-        b_fc1 = bias_variable([_out_width3], name='b_fc1', )
+                out3 = h_conv2
 
-        h_pool2_flat = tf.reshape(in3, [-1, _height3 * _width3 * _in_channels3], name='h_pool2_flat', )
-        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1, name='h_fc1', )  # 这不再卷积,直接矩阵乘
+            # S4
+            with tf.name_scope('S4') as _:
+                in4 = out3
 
-        out3 = h_fc1  # shape[_example_cnt, _out_width3]
+                h_pool2 = max_pool(in4, CNNTrainer.POOL_SHAPE, name='h_pool2', )
 
-        # Dropout
-        tmp_in = out3
-        h_fc1_drop = tf.nn.dropout(tmp_in, keep_prob, name='h_fc1_drop', )
-        tmp_out = h_fc1_drop
-        out3 = tmp_out
+                out4 = h_pool2
 
-        # Readout Layer
-        in4 = out3  # shape[_example_cnt, _in_width4]
-        _in_width4 = in4.get_shape()[1].value
-        _out_width4 = target_class_cnt
+            # F5, Densely Connected Layer(Full Connected Layer)
+            with tf.name_scope('F5') as _:
+                in5 = out4  # shape[_example_cnt, _height3, _width3, _in_channels3]
+                _height3, _width3 = in5.get_shape()[1].value, in5.get_shape()[2].value
+                _in_channels3 = in5.get_shape()[3].value
+                _out_width3 = 1024
 
-        W_fc2 = weight_variable([_in_width4, _out_width4], name='W_fc2', )
-        b_fc2 = bias_variable([_out_width4], name='b_fc2', )
+                W_fc1 = weight_variable([_height3 * _width3 * _in_channels3, _out_width3], name='W_fc1', )
+                b_fc1 = bias_variable([_out_width3], name='b_fc1', )
+                h_pool2_flat = tf.reshape(in5, [-1, _height3 * _width3 * _in_channels3], name='h_pool2_flat', )
+                h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1, name='h_fc1', )  # 这不再卷积,直接矩阵乘
 
-        y_conv = tf.add(tf.matmul(in4, W_fc2), b_fc2, name='y_conv', )
+                out5 = h_fc1  # shape[_example_cnt, _out_width3]
 
-        out4 = y_conv  # shape[_example_cnt, _out_width4]
+            # D6, Dropout
+            with tf.name_scope('D6') as _:
+                in6 = out5
 
-        # Train definition
-        cross_entropy = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv), name='cross_entropy', )
-        train_per_step = tf.train.AdamOptimizer(1e-5).minimize(cross_entropy, name='train_per_step', )
+                h_fc1_drop = tf.nn.dropout(in6, keep_prob, name='h_fc1_drop', )
 
-        # Evaluate definition
-        correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1), name='correct_prediction', )
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy', )
+                out6 = h_fc1_drop
+
+            # Output Layer
+            with tf.name_scope('OutputLayer') as _:
+                in7 = out6  # shape[_example_cnt, _in_width4]
+                _in_width4 = in7.get_shape()[1].value
+                _out_width4 = target_class_cnt
+                W_fc2 = weight_variable([_in_width4, _out_width4], name='W_fc2', )
+                b_fc2 = bias_variable([_out_width4], name='b_fc2', )
+                y = tf.add(tf.matmul(in7, W_fc2), b_fc2, name='y', )
+
+            out7 = y  # shape[_example_cnt, _out_width4]
+
+        with tf.name_scope('trainer') as _:
+            # Train definition
+            loss = tf.reduce_mean(
+                tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y), name='loss', )
+            train_per_step = tf.train.AdamOptimizer(1e-5).minimize(loss, name='train_per_step', )
+            loss_summary = tf.summary.scalar('loss', loss)
+
+        with tf.name_scope('evaluator') as _:
+            # Evaluate definition
+            correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1), name='correct_prediction', )
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy', )
+            accuracy_summary = tf.summary.scalar('accuracy', accuracy)
 
         return train_per_step, accuracy
 
@@ -237,6 +265,9 @@ class CNNTrainer(object):
             train_data=None, test_data=None, target_class_cnt=None,
             x=None, y_=None, keep_prob=None,
     ):
+        summaries = tf.summary.merge_all()
+        summay_log_dir_path = 'tmp/model_visualization'
+        summary_writer = tf.summary.FileWriter(logdir=summay_log_dir_path, graph=sess.graph)
         sess.run(tf.global_variables_initializer())
         for i in range(iteration):
             batch_train = random_sample(train_data, batch_size)
@@ -261,6 +292,9 @@ class CNNTrainer(object):
                     if test_accuracy > 0.83 and train_accuracy > 0.83:  return
             _X, _Y = CNNTrainer.__format_inputs(batch_train, target_class_cnt, )
             train_per_step.run(feed_dict={x: _X, y_: _Y, keep_prob: CNNTrainer.KEEP_PROB}, session=sess)
+            summaries_result = sess.run(summaries, feed_dict={x: _X, y_: _Y, keep_prob: CNNTrainer.KEEP_PROB}, )
+            summary_writer.add_summary(summaries_result, global_step=i)
+        summary_writer.close()
 
     @staticmethod
     def evaluate(
