392aa60cc529a23996c2e85f3bbc16ca444991d7
forienlauo <244382021@qq.com>
2017-11-16 11:15:22 +0800

    Add exit_code for module prepare

--- src/prepare.py
+++ src/prepare.py
@@ -1,14 +1,14 @@
-import conf
-import os
 import logging
-from conf import DataType
-from src.util.filter.InfoFilter import InfoFilter
-from src.util.filter.RecordFilter import RecordFilter
-from src.util.feature.FeatureCNN import FeaturesCNN
+import os
 import shutil
 
-class Prepare:
+from conf import DataType
+from src.util.feature.FeatureCNN import FeaturesCNN
+from src.util.filter.InfoFilter import InfoFilter
+from src.util.filter.RecordFilter import RecordFilter
 
+
+class Prepare:
     @staticmethod
     def print_usage():
         print("""
@@ -29,7 +29,6 @@ class Prepare:
         info_data_path, record_data_path, output_data_path = argv[0], argv[1], argv[2]
         do_clean = argv[3]
 
-
         if not os.path.exists(info_data_path):
             logging.info('info_data_path do not exists: "%s"', info_data_path)
             return -1
@@ -47,8 +46,7 @@ class Prepare:
                 shutil.rmtree(output_data_path)
             os.makedirs(output_data_path)
 
-
-        normal_info = InfoFilter(DataType.normal.name, info_data_path , output_data_path).process()
+        normal_info = InfoFilter(DataType.normal.name, info_data_path, output_data_path).process()
         normal_record = RecordFilter(DataType.normal.name, record_data_path, output_data_path).process()
         normal_feature = FeaturesCNN(DataType.normal.name, output_data_path)
         normal_feature.set_info_filter(normal_info)
@@ -56,26 +54,20 @@ class Prepare:
         normal_feature.process()
 
         fraud_info = InfoFilter(DataType.fraud.name, info_data_path, output_data_path).process()
-        fraud_record = RecordFilter(DataType.fraud.name, record_data_path , output_data_path).process()
+        fraud_record = RecordFilter(DataType.fraud.name, record_data_path, output_data_path).process()
         fraud_feature = FeaturesCNN(DataType.fraud.name, output_data_path)
         fraud_feature.set_info_filter(fraud_info)
         fraud_feature.set_record_filter(fraud_record)
         fraud_feature.process()
 
+        return 0
+
 
 if __name__ == '__main__':
     argv = [
-            '/Users/mayuchen/Documents/Python/Repository/DL/Other/UserBehaviorMining/resource/raw/',
-            '/Users/mayuchen/Documents/Python/Repository/DL/Other/UserBehaviorMining/resource/raw/',
-            '/Users/mayuchen/Documents/Python/Repository/DL/Other/UserBehaviorMining/resource/data/',
-            '1'
-            ]
+        '/Users/mayuchen/Documents/Python/Repository/DL/Other/UserBehaviorMining/resource/raw/',
+        '/Users/mayuchen/Documents/Python/Repository/DL/Other/UserBehaviorMining/resource/raw/',
+        '/Users/mayuchen/Documents/Python/Repository/DL/Other/UserBehaviorMining/resource/data/',
+        '1'
+    ]
     Prepare.prepare(argv)
-
-
-
-
-
-
-
-
